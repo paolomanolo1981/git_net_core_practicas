f313d0d89f2fbc9c4c6b0e29e9b25d2cc2b4399e
paolomanolo1981 <paolo.manolo28@gmail.com>
2021-11-15 18:41:37 -0500

    c

---
+++ 01.consola/consola/MIConsolaNetCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MIConsolaNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "Hola Mundo";
+            Console.WriteLine(cadena);
+        }
+    }
+}
---
+++ 01.consola/consola/consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

df44bf69fa490949d964fa5565eaf9db00d1921e
paolomanolo1981 <paolo.manolo28@gmail.com>
2021-11-16 02:19:55 -0500

    c

---
+++ 01.consola/consola/CursoCSharp_3_conversiones/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Convert;
+
+namespace CursoCSharp_3_conversiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "hola";
+            int numero=5;
+
+            cadena = numero.ToString();
+
+            //implicitos
+            int numeroEntero = 3457;
+            long numeroLong = numeroEntero;
+
+
+            //explicitos
+            double numeroDouble = 12.56;
+            int numeroEnteroDouble = (int)numeroDouble;
+
+            Console.WriteLine(numeroEnteroDouble);
+
+            double doubleValor = 8.45;
+            int enteroValor = ToInt32(doubleValor);
+
+            //definidos por el usuario
+
+            // con clases de asistentes
+
+
+        }
+
+         
+    }
+}
--- 01.consola/consola/CursoCSharp_3_operadores/Program.cs
+++ 01.consola/consola/CursoCSharp_3_operadores/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
+using static System.Console;
 namespace CursoCSharp_3_operadores
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("hay " + args.Length + " elementos");
+
+            int[] arrayEnteros = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] arrayEnteros2 = new int[5];
+
+            int[] arrayEnteros3 ;
+
+            int[,] arrayDeEnteros2Dimensiones = new int[2, 2];
+            int[,] arrayDeEnteros2Dimensiones2 = { { 2,2},{ 3, 6 } };
+            int[][] arrayDeEnterosEscalonados = new int[6][];
+             arrayDeEnterosEscalonados[0]= new int[4] { 3,6,7,2};
+            arrayDeEnterosEscalonados[1] = new int[2] { 1,3 };
+
+
+            for (int i = 0; i < arrayEnteros.Length; i++)
+            {
+                Console.WriteLine(arrayEnteros[i]);
+            }
         }
     }
 }

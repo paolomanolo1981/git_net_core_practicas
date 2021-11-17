06f5fb88cbb71b47148b3d07fe41b88f85260d8a
paolomanolo1981 <paolo.manolo28@gmail.com>
2021-11-17 03:09:17 -0500

    c

---
+++ 01.consola/consola/intro_colecciones/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace intro_colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- 01.consola/consola/operaciones_cadena/Program.cs
+++ 01.consola/consola/operaciones_cadena/Program.cs
@@ -45,6 +45,40 @@ namespace operaciones_cadena
 
             Console.WriteLine(frase3);
 
+            //borrar cadena
+            var posicion2 = frase3.LastIndexOf("enorme");
+            var frase4= frase3.Remove(posicion2, 7);
+
+            Console.WriteLine(frase4);
+
+
+            //reemplazar cadena
+            var frase5 =  frase.Replace("mundo", "luna");
+            Console.WriteLine(frase5);
+
+
+            //dividir cadena
+            var palabras = frase.Split(" ");
+            foreach(var palabra in palabras)
+            {
+                Console.WriteLine(palabra);
+            }
+
+
+            //mayusculas y minuscula
+            string GRU = villano.ToUpper();
+            string esbirros2 = esbirros.ToLower();
+            Console.WriteLine(GRU);
+            Console.WriteLine(esbirros2);
+
+            //podar cadenas
+            string espaciosDelanteros = "   palabras";
+            string espaciosFinales = "palabras finales     ";
+            string espacios  = "     palabras finales     ";
+
+            Console.WriteLine(espaciosDelanteros.TrimStart() +  "-");
+            Console.WriteLine(espaciosFinales.TrimEnd() + "-");
+            Console.WriteLine(espacios.Trim() + "-");
 
             Console.ReadLine();
         }

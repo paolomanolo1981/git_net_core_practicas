1d6f4440fe95942e39a375d7f993f9b5e421669e
epecho <epecho@minsa.gob.pe>
2021-11-17 21:58:08 -0500

    c

---
+++ 01.consola/consola/seccion_7_array_list/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace seccion_7_array_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList lista = new ArrayList();
+
+            lista.Add(1);
+            lista.Add("Batman");
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            var elemento = lista[0];
+            Console.WriteLine(elemento);
+
+            var elemento1 = lista[1];
+            Console.WriteLine(elemento1);
+
+            int total = lista.Count;
+            Console.WriteLine(total);
+
+            lista.Insert(1, "SuperMan");
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            lista.RemoveAt(1);
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

7c7e3b574473bc71fcba904b7d45ee1249b74374
paolomanolo1981 <paolo.manolo28@gmail.com>
2021-11-18 03:31:02 -0500

    c

--- 01.consola/consola/colas/Program.cs
+++ 01.consola/consola/colas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace colas
 {
@@ -6,7 +7,58 @@ namespace colas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            //FIFO=> primero en entrar, primero en salir 
+            Queue cola = new Queue();
+            cola.Enqueue(3);
+             cola.Enqueue(5);
+            cola.Enqueue(1);
+            foreach (var c in cola)
+            {
+                Console.WriteLine("0 " + c);
+            }
+
+            //extraer un valor de la cola
+            /* var valorCola = cola.Dequeue();
+            Console.WriteLine("valorCola " + valorCola);
+
+              valorCola = cola.Dequeue();
+            Console.WriteLine("valorCola    " + valorCola);
+           */
+            //para observar el valor()
+            var valor = cola.Peek();
+            Console.WriteLine("PEEK "+valor);
+            //recorrer cola
+            foreach (var c in cola)
+            {
+                Console.WriteLine("a " + c);
+            }
+
+
+
+            //comprobar si existe un valor
+            if (cola.Contains(5))
+            {
+                Console.WriteLine("existe 5");
+            }else
+            {
+                Console.WriteLine("no existe 5");
+            }
+
+            //borrar valores de la cola
+            cola.Clear();
+            cola.Enqueue(30);
+            cola.Enqueue(50);
+            //conocer cuantos elementos hay en la cola
+            int elementos = cola.Count;
+
+            //recorrer cola
+            foreach(var c in cola)
+            {
+                Console.WriteLine("e " + c);
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ 01.consola/consola/my_hash/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+namespace my_hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Hashtable hash = new Hashtable();
+
+            hash.Add("Alejandro", 1.22);
+            hash.Add("Rodrigo", 5.21);
+            hash.Add("Mirima", 9.62);
+
+            //recorriendo el hashtable
+            foreach(DictionaryEntry item in hash)
+            {
+                Console.WriteLine($"item: {item.Key} valor: {item.Value}"  );
+            }
+
+            var valor = hash["Alejandro"];
+            Console.WriteLine("VALOR " + valor);
+
+            //contar
+            var total = hash.Count;
+            Console.WriteLine("total " + total);
+            if (hash.ContainsKey("Alejandro"))
+            {
+                Console.WriteLine("Alejandro " );
+            }
+
+            if (hash.ContainsValue(5.21))
+            {
+                Console.WriteLine("5.21 ");
+            }
+
+            //7/eliminar
+            hash.Remove("Alejandro");
+            //recorriendo el hashtable
+            foreach (DictionaryEntry item in hash)
+            {
+                Console.WriteLine($"Eliminado:=> item: {item.Key} valor: {item.Value}");
+            }
+
+            //limpiar 
+            hash.Clear();
+        }
+    }
+}
